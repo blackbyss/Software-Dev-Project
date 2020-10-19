7e6a228b41af811ddffbe365b54e941cf60d6973
AllarKalina <allarkalina@gmail.com>
2020-10-19 19:29:04 +0300

    Warehouse GUI UI/Add/Remove/Edit <TAR120> <TAR138> <TAR122> <TAR125> <TAR126> <TAR129> <TAR131> <TAR132>

--- SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/StockController.java
+++ SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/StockController.java
@@ -1,13 +1,17 @@
 package ee.ut.math.tvt.salessystem.ui.controllers;
 
-import com.sun.javafx.collections.ObservableListWrapper;
+import ee.ut.math.tvt.salessystem.dao.InMemorySalesSystemDAO;
 import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
 import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
+import ee.ut.math.tvt.salessystem.logic.Warehouse;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,10 +20,31 @@ public class StockController implements Initializable {
 
     private final SalesSystemDAO dao;
 
+
+    @FXML
+    private TextField insertBar;
+
+    @FXML
+    private TextField insertAmount;
+
+    @FXML
+    private TextField insertName;
+
+    @FXML
+    private TextField insertPrice;
+
     @FXML
     private Button addItem;
+
     @FXML
     private TableView<StockItem> warehouseTableView;
+
+    @FXML
+    private Button refreshButton;
+
+    @FXML
+    private Button removeButton;
+
 
     public StockController(SalesSystemDAO dao) {
         this.dao = dao;
@@ -28,16 +53,44 @@ public class StockController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         refreshStockItems();
-        // TODO refresh view after adding new items
+        addItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                addStockItem(Long.parseLong(insertBar.getText()), Integer.parseInt(insertAmount.getText())
+                        , String.valueOf(insertName.getText()), Double.parseDouble(insertPrice.getText()));
+                refreshStockItems();
+            }
+        });
+        refreshButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                refreshButtonClicked();
+            }
+        });
+        removeButton.setOnAction(e -> {
+            StockItem valitud = warehouseTableView.getSelectionModel().getSelectedItem();
+            warehouseTableView.getItems().remove(valitud);
+        });
     }
 
     @FXML
     public void refreshButtonClicked() {
         refreshStockItems();
+        System.out.println("Värskendab");
     }
 
     private void refreshStockItems() {
         warehouseTableView.setItems(FXCollections.observableList(dao.findStockItems()));
         warehouseTableView.refresh();
     }
+
+    private void addStockItem(Long id, int amount, String name, double price) {
+        StockItem stockItem = new StockItem(id, name, "", price, amount);
+        dao.saveStockItem(stockItem);
+    }
+
+    private void removeStockItem(){
+
+    }
+
 }
